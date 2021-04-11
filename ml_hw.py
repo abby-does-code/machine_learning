46ebdc675c250df8d690d3586acf9426fb72903f
Abby Brown <abby_brown3@baylor.edu>
2021-04-11 13:28:59 -0500

    -YIKES! Got a weird error; emailed Cameron

--- ml_hw.py
+++ ml_hw.py
@@ -1,8 +1,7 @@
-"""1.  Create a Seaborn pairplot graph (the book has an example in Unsupervised Machine Learning for the Iris Dataset Section 15.7.3) for the California Housing dataset. Try the Matplotlib features for panning and zooming the diagram. These are accessible via the icons in the Matplotlib window.
+"""1.  Create a Seaborn pairplot graph (the book has an example in Unsupervised Machine Learning for the Iris Dataset Section 15.7.3) for the California Housing dataset. Try the Matplotlib features for panning and zooming the diagram. These are accessible via the icons in the Matplotlib window."""
 
- 
 
-2. Re-implement the simple linear regression case study of Section 15.4 using the average yearly temperature data. How does the temperature trend compare to the average January high temperatures?"""
+"""2. Re-implement the simple linear regression case study of Section 15.4 using the average yearly temperature data. How does the temperature trend compare to the average January high temperatures?"""
 
 # Start#
 from sklearn.datasets import fetch_california_housing
@@ -23,7 +22,10 @@ pd.set_option("display.width", None)  # Auto-detect the display
 
 california_df = pd.DataFrame(california.data, columns=california.feature_names)
 
+
 print(california_df.describe())
+
+california_df["MedHouseValue"] = pd.Series(california.target)
 
 ## Created a data frame because it looks like that's what the graph is built off of below
 
@@ -34,9 +36,12 @@ sns.set_style("whitegrid")
 
 # Code below uses pairplot to create a grid of graphs plotting each feature against each itself and the other specified features
 
-grid = sns.pairplot(
+"""grid = sns.pairplot(
     data=california_df, vars=california_df.columns[0:4], hue="MedHouseValue"
-)
+)"""
 
-"""OR 
-grid = sns.pairplot(data=california_df, vars=california_df.columns[0:4])"""
+OR
+grid = sns.pairplot(data=california_df, vars=california_df.columns[0:4])
+
+
+"""2. Re-implement the simple linear regression case study of Section 15.4 using the average yearly temperature data. How does the temperature trend compare to the average January high temperatures?"""
