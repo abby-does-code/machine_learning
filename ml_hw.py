278571e75c31e7413ee6c040f4ec4f3703d468f1
Abby Brown <abby_brown3@baylor.edu>
2021-04-11 19:55:37 -0500

    -Working through HW; something's wrong with lambda function in line 89

--- ml_hw.py
+++ ml_hw.py
@@ -23,9 +23,9 @@ pd.set_option("display.width", None)  # Auto-detect the display
 california_df = pd.DataFrame(california.data, columns=california.feature_names)
 
 
-print(california_df.describe())
+# print(california_df.describe())
 
-california_df["MedHouseValue"] = pd.Series(california.target)
+"""california_df["MedHouseValue"] = pd.Series(california.target)
 
 ## Created a data frame because it looks like that's what the graph is built off of below
 
@@ -36,12 +36,62 @@ sns.set_style("whitegrid")
 
 # Code below uses pairplot to create a grid of graphs plotting each feature against each itself and the other specified features
 
-"""grid = sns.pairplot(
-    data=california_df, vars=california_df.columns[0:4], hue="MedHouseValue"
-)"""
+grid = sns.pairplot(
+    data=california_df, vars=california_df.columns[0:4],palette="cool", hue="MedHouseValue", legend = False
+)
 
 OR
-grid = sns.pairplot(data=california_df, vars=california_df.columns[0:4])
+grid = sns.pairplot(data=california_df, vars=california_df.columns[0:4])"""
 
 
 """2. Re-implement the simple linear regression case study of Section 15.4 using the average yearly temperature data. How does the temperature trend compare to the average January high temperatures?"""
+
+nyc = pd.read_csv("ave_hi_nyc_jan_1895-2018.csv")
+
+nyc.columns = ["Date", "Temperature", "Anomaly"]
+
+nyc.Date = nyc.Date.floordiv(100)
+
+print(nyc.head(3))
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    nyc.Date.values.reshape(-1, 1), nyc.Temperature.values, random_state=11
+)
+
+print(X_train.shape)
+print(X_test.shape)
+
+from sklearn.linear_model import LinearRegression
+
+linear_regr = LinearRegression()
+
+linear_regr.fit(X=X_train, y=y_train)
+
+print(linear_regr.coef_)
+
+
+# Testing our model
+##We use these predictions vs. expected lines to test how our model is doing
+predicted = linear_regr.predict(X_test)
+
+expected = y_test
+
+for p, e in zip(predicted[::5], expected[::5]):
+    print(f"predicted: {p:.2f},expected: {e:.2f}")
+
+# Based on the output from this statement, our model SUCKS
+##...either that or I'm doing something horribly wrong.
+
+# NOW we're moving on to comparing two different data sets! How exciting.
+
+predict = lambda x: linear_regr.coef_ * x + linear_regr.intercept_
+
+print(predict(2019))
+# This is alarming:
+# OUTPUT : 3005.254
+
+print(predict(1890))
+# OUTPUT: 2813
+
